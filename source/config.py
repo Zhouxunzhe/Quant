92b0bedd72ca472069e85c22bf2b6f5f64b502ab
Xunzhe Zhou <120677956+Zhouxunzhe@users.noreply.github.com>
2024-05-14 20:27:12 +0800

    Update config.py

--- source/config.py
+++ source/config.py
@@ -1,4 +1,4 @@
 base_url = 'https://testnet.binancefuture.com'
-api_key = '99124d551263bc64b32bf0c31c751bcb5fcd7468cd50e9eafa1b02bb73f9a92c'
-api_secret = 'c89790ce5c11ae3f45d53b09ab8e949cba37871a34814772e38a68f1407d1389'
+api_key = ''
+api_secret = ''
 SYMBOL = 'BTCUSDT'
